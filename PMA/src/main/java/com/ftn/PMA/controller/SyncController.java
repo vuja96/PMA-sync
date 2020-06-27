2ce6bf609a0c7aac779b7bb5f89bbfad78cca3a2
trifko96 <trifko96@gmail.com>
2020-06-27 19:29:39 +0200

    provera uspesnosti sinhronizacije

--- PMA/src/main/java/com/ftn/PMA/controller/SyncController.java
+++ PMA/src/main/java/com/ftn/PMA/controller/SyncController.java
@@ -3,6 +3,8 @@ package com.ftn.PMA.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +28,7 @@ import com.ftn.PMA.repository.MealRepository;
 import com.ftn.PMA.repository.UserRepository;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("")
 public class SyncController {
 
 	@Autowired
@@ -54,59 +56,115 @@ public class SyncController {
 	UserRepository userRepository;
 	
 	@RequestMapping(value = "/syncActivity", method = RequestMethod.POST)
-	public void syncActivity(@RequestBody List<Activity> a) {
+	public ResponseEntity<List<Activity>> syncActivity(@RequestBody List<Activity> a) {
+		boolean tmp = false;
 		for(Activity ac : a) {
-			activityRepository.save(ac);
+			Activity result = activityRepository.save(ac);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/syncActivityType", method = RequestMethod.POST)
-	public void syncActivityType(@RequestBody List<ActivityType> acType) {
+	public ResponseEntity<List<ActivityType>> syncActivityType(@RequestBody List<ActivityType> acType) {
+		boolean tmp = false;
 		for(ActivityType act : acType) {
-			activityTypeRepository.save(act);
+			ActivityType result = activityTypeRepository.save(act);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/syncDailySummary", method = RequestMethod.POST)
-	public void syncDailySummary(@RequestBody List<DailySummary> daily) {
+	public ResponseEntity<List<DailySummary>> syncDailySummary(@RequestBody List<DailySummary> daily) {
+		boolean tmp = false;
 		for(DailySummary ds : daily) {
-			dailyRepository.save(ds);
+			DailySummary result = dailyRepository.save(ds);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/syncGrocery", method = RequestMethod.POST)
-	public void syncGrocery(@RequestBody List<Grocery> grocery) {
+	public ResponseEntity<List<Grocery>> syncGrocery(@RequestBody List<Grocery> grocery) {
+		boolean tmp = false;
 		for(Grocery g : grocery) {
-			groceryRepository.save(g);
+			Grocery result = groceryRepository.save(g);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/syncGroceryAndAmount", method = RequestMethod.POST)
-	public void syncGroceryAndAmount(@RequestBody List<GroceryAndAmountPair> grocery) {
+	public ResponseEntity<List<GroceryAndAmountPair>> syncGroceryAndAmount(@RequestBody List<GroceryAndAmountPair> grocery) {
+		boolean tmp = false;
 		for(GroceryAndAmountPair g : grocery) {
-			groceryAndAmountRepository.save(g);
+			GroceryAndAmountPair result = groceryAndAmountRepository.save(g);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/syncLocation", method = RequestMethod.POST)
-	public void syncLocation(@RequestBody List<Location> location) {
+	public ResponseEntity<List<Location>> syncLocation(@RequestBody List<Location> location) {
+		boolean tmp = false;
 		for(Location l : location) {
-			locationRepository.save(l);
+			Location result = locationRepository.save(l);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/syncMeal", method = RequestMethod.POST)
-	public void syncMeal(@RequestBody List<Meal> meal) {
+	public ResponseEntity<List<Meal>> syncMeal(@RequestBody List<Meal> meal) {
+		boolean tmp = false;
 		for(Meal m : meal) {
-			mealRepository.save(m);
+			Meal result = mealRepository.save(m);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@RequestMapping(value = "/syncUser", method = RequestMethod.POST)
-	public void syncUser(@RequestBody List<User> user) {
+	public ResponseEntity<List<User>> syncUser(@RequestBody List<User> user) {
+		boolean tmp = false;
 		for(User u : user) {
-			userRepository.save(u);
+			User result = userRepository.save(u);
+			if(result == null)
+				tmp = true;
 		}
+		if(tmp == false)
+			return new ResponseEntity<>(HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 }
